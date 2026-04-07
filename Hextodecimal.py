d2022af8e7178065e1e165ea706e8d2acc22d674
Narayan Paravasthu <pniy1989@gmail.com>
2026-04-07 02:23:53 -0700

    Add files via upload

---
+++ Hextodecimal.py
@@ -0,0 +1,12 @@
+hexnumbers = {'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'A':10,'B':11,'C':12,'D':13,'E':14,'F':15}
+def hextodec(hexnum):
+    for char in hexnum:
+        if char not in hexnumbers:
+            return None
+    converted = 0
+    exponent = len(hexnum) - 1
+    for char in hexnum:
+        converted = converted + (hexnumbers[char] * (16 ** exponent))
+        exponent = exponent - 1
+    return converted         
+
