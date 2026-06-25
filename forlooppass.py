e93d20c59aa1cb8f7d5852e19ebb1ffdac48dbb5
Narayan Paravasthu <pniy1989@gmail.com>
2026-06-25 10:06:16 -0700

    Add files via upload

---
+++ forlooppass.py
@@ -0,0 +1,11 @@
+dictionary = {'a':['aadvark','antelope'],'b':['bat','bear'],'c':['cat','civet'],'d':['dog','duck']}
+for letter, animals in dictionary.items():
+	print(letter)
+for letter, animals in dictionary.items():
+    print(animals)
+for letter, animals in dictionary.items():
+    print(letter, animals)
+for letter, animals in dictionary.items():
+    print(dictionary.items())
+for letter, animals in dictionary.items():
+    pass
